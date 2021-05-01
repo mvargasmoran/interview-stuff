ae3cbd073d728babc7ecd7d0c242c9e5af015899
mvargasmoran <mvargasmoran@gmail.com>
2021-05-01 05:18:08 -0400

    Add a new exercise that mentions (O)n BTW

---
+++ daily-coding-problems/compute-median-unordered.js
@@ -0,0 +1,18 @@
+/*
+ * This problem was asked by Dropbox.
+ *
+ * Create an algorithm to efficiently compute the approximate median of a
+ * list of numbers.
+ *
+ * More precisely, given an unordered list of N numbers, find an element whose
+ * rank is between N / 4 and 3 * N / 4, with a high level of certainty,
+ * in less than O(N) time.
+ *
+ */
+
+let unorderedList = [9,8,7,6,5,4,3,2,1,];
+const median = (array) => {
+
+};
+
+module.exports = median;
---
+++ daily-coding-problems/compute-median-unordered.test.js
@@ -0,0 +1,4 @@
+const median = require('./compute-median-unordered.js');
+it('should compute median in (O)n', () => {
+  //expect(median()).toBe(2);
+});
